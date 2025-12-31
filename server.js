82aff1de5459899afee3feb1911888a147c18b47
Yussef Makhlouf <yussef.ali.it@gmail.com>
2025-12-31 22:00:47 +0200

    feat: broaden CORS origin to all, ignore environment files, and update main site URL in config

--- server.js
+++ server.js
@@ -28,10 +28,7 @@ app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 // CORS
 app.use(cors({
     origin: [
-        process.env.FRONTEND_URL || 'http://localhost:3001',
-        process.env.MAIN_SITE_URL || 'http://localhost:3000',
-        'http://localhost:3000',
-        'http://localhost:3001'
+    '*' 
     ],
     credentials: true
 }));
